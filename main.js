de99b04cd76c374440e3bb12b7eb429ab231edd2
Simon Prato <simonprato11@gmail.com>
2021-12-03 14:09:43 +0000

    btn click function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.search-btn')
 const input = document.getElementById('search-input')
 const id = 'de35f7b7'
+const container = document.querySelector('.info-box')
 
 async function fetchRecipe() {
     let food = handleInputChange();
@@ -17,7 +18,33 @@ function handleInputChange() {
 }
 async function handleRecipeClick() {
     let info = await fetchRecipe()
-    console.log(info);
+    const recipeArray = info.hits;
+    const recipes = recipeArray.map(function (recipe){
+        return 
+        `<img class="info-left" src="https://via.placeholder.com/150" alt="food picture">
+        <div class="info-middle">
+          <div>I am pasta</div>
+          <div>I am brief description I am brief description I am brief description I am brief description</div>
+          <div>40min</div>
+          <div>384kcal</div>
+        </div>
+        <div class="info-right">
+          <div>reviews</div>
+          <div>allergen symbol</div>
+        </div>`
+    })
+    let showRecipes = recipes.join('')
+    container.innerHTML = `${show}`
+    console.log(info.hits[0].recipe.label);
 }
-
+// info.hits[0].recipe.label
 btn.addEventListener('click', handleRecipeClick)
+
+
+// const showItems = function (){
+//     const list = array.map( function (item){
+//     return <li>${item.text}</li>
+//     })
+// let listFinal = list.join('');
+// ol.innerHTML = `${listFinal}`
+// }
